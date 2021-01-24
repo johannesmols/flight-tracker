71d4cd2d88bc2433b12774de2cc0ec7550634024
Johannes Mols <johannes.mols@googlemail.com>
2021-01-24 10:31:53 +0100

    Improved colors

--- js/main.js
+++ js/main.js
@@ -93,12 +93,8 @@ function lookUpAirport(identOrCoords) {
 }
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+    var colors = ['#7F3C8D','#11A579','#3969AC','#F2B701','#E73F74','#80BA5A','#E68310','#008695','#CF1C90','#f97b72','#4b4b8f','#A5AA99'];
+    return colors[Math.floor(Math.random() * colors.length)];
 }
 
 function addMarkerToMap(coordinates, color) {
